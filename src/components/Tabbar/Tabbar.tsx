8bb5b007f8cf2892b3a4a045c6c875ca5feac4c7
Måns Nilsson <mans.n@hotmail.com>
2020-04-29 22:13:10 +0200

    tab bar visual

---
+++ src/components/Tabbar/Tabbar.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export default function Tabbar() {
+  return (
+    <div id="tab-bar">
+      <div>
+        <button>
+          <img src="/icons/student-512.png" alt="profile"></img>
+        </button>
+      </div>
+      <div>
+        <button>
+          <img src="/icons/calculator-5-512.png" alt="profile"></img>
+        </button>
+      </div>
+      <div>
+        <button>
+          <img src="/icons/chat-4-512.png" alt="profile"></img>
+        </button>
+      </div>
+      <div>
+        <button>
+          <img src="/icons/note-2-512.png" alt="profile"></img>
+        </button>
+      </div>
+    </div>
+  );
+}
