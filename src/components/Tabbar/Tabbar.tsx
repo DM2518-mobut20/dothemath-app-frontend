4c2a8bb52e6d3ed46e59633fa0c831fd42ed1f33
Måns Nilsson <mans.n@hotmail.com>
2020-04-30 14:57:21 +0200

    Added Tabbar
    added dependancy react-router-dom
    
    
    Added empty Profile Component
    
    
    Profile loads when tab is clicked
    
    
    Added empty Calculator Component
    
    
    Added empty formula sheet Component
    
    
    made the chat icon link to App
    
    
    Entire buttons are clickable not just icons
    
    
    fixed change requests

---
+++ src/components/Tabbar/Tabbar.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import Profile from '../Profile/Profile';
+import Calculator from '../Calculator/Calculator';
+import Formulasheet from '../Formulasheet/Formulasheet';
+import App from '../../App';
+
+export default function Tabbar() {
+  return (
+    <Router>
+      <div id="site-wrapper">
+        <Switch>
+          <Route exact path="/profile" component={Profile} />
+          <Route exact path="/calculator" component={Calculator} />
+          <Route exact path="/formulasheet" component={Formulasheet} />
+          <Route exact path="/chat" component={App} />
+        </Switch>
+        <div id="tab-bar">
+          <div>
+            <Link to="/profile">
+              <button>
+                <img src="/icons/student-512.png" alt="profile"></img>
+              </button>
+            </Link>
+          </div>
+          <div>
+            <Link to="/calculator">
+              <button>
+                <img src="/icons/calculator-5-512.png" alt="calculator"></img>
+              </button>
+            </Link>
+          </div>
+          <div>
+            <Link to="/chat">
+              <button>
+                <img src="/icons/chat-4-512.png" alt="chat"></img>
+              </button>
+            </Link>
+          </div>
+          <div>
+            <Link to="/formulasheet">
+              <button>
+                <img src="/icons/note-2-512.png" alt="formula sheet"></img>
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </Router>
+  );
+}
