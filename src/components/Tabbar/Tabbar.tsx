1cf3093d4630cb81752f521826a98fe42e85b8fa
Måns Nilsson <mans.n@hotmail.com>
2020-04-30 10:01:12 +0200

    Profile loads when tab is clicked

--- src/components/Tabbar/Tabbar.tsx
+++ src/components/Tabbar/Tabbar.tsx
@@ -1,28 +1,39 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import Profile from '../Profile/Profile';
 
 export default function Tabbar() {
   return (
-    <div id="tab-bar">
-      <div>
-        <button>
-          <img src="/icons/student-512.png" alt="profile"></img>
-        </button>
+    <Router>
+      <div id="site-wrapper">
+        <Switch>
+          <Route exact path="/profile" component={Profile}></Route>
+        </Switch>
+        <div id="tab-bar">
+          <div>
+            <button>
+              <Link to="/profile">
+                <img src="/icons/student-512.png" alt="profile"></img>
+              </Link>
+            </button>
+          </div>
+          <div>
+            <button>
+              <img src="/icons/calculator-5-512.png" alt="profile"></img>
+            </button>
+          </div>
+          <div>
+            <button>
+              <img src="/icons/chat-4-512.png" alt="profile"></img>
+            </button>
+          </div>
+          <div>
+            <button>
+              <img src="/icons/note-2-512.png" alt="profile"></img>
+            </button>
+          </div>
+        </div>
       </div>
-      <div>
-        <button>
-          <img src="/icons/calculator-5-512.png" alt="profile"></img>
-        </button>
-      </div>
-      <div>
-        <button>
-          <img src="/icons/chat-4-512.png" alt="profile"></img>
-        </button>
-      </div>
-      <div>
-        <button>
-          <img src="/icons/note-2-512.png" alt="profile"></img>
-        </button>
-      </div>
-    </div>
+    </Router>
   );
 }
