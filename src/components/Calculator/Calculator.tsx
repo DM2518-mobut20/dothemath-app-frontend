63810a59e47ff0d2dfde42e34f3aae0153f8ed9b
Måns Nilsson <mans.n@hotmail.com>
2020-05-01 15:59:58 +0200

    Calculator is in place but not styled

--- src/components/Calculator/Calculator.tsx
+++ src/components/Calculator/Calculator.tsx
@@ -1,5 +1,17 @@
 import React from 'react';
 
 export default function Calculator() {
-  return <div>Calculator</div>;
+  return (
+    <div id="calculator-wrapper">
+      <iframe
+        title="Calculator"
+        width="100%"
+        id="calculator"
+        src="https://web2.0calc.com/widgets/416877538f92/"
+        scrolling="no"
+      >
+        {' '}
+      </iframe>
+    </div>
+  );
 }
