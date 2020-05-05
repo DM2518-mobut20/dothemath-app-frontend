52315cb237350d0e9cb1ecbce730b4c4aab513ad
Måns Nilsson <mans.n@hotmail.com>
2020-05-05 20:50:33 +0200

    Add profile item list with temporary data
    Fix padding bottom on profile list
    
    Remove whitespace

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,5 +1,50 @@
 import React from 'react';
+import Item from './Item';
 
 export default function Profile() {
-  return <div>Profile</div>;
+  // Temporary posts-data before we get real data from the backend
+  const posts = [
+    {
+      id: 1,
+      checkmark: false,
+      text: `Jag har problem med denna frågan, vad är x^2 + 4x + 39 = 19. Jag har
+             försökt använda pq-formeln men jag får inte...`,
+      imgurl:
+        'https://www.incimages.com/uploaded_files/image/970x450/getty_470493341_20001333200092800_398689.jpg',
+    },
+    {
+      id: 2,
+      checkmark: true,
+      text: `Jag har problem med denna frågan, vad är x^2 + 4x + 39 = 19. Jag har
+            försökt använda pq-formeln men jag får inteJag har problem med denna frågan, vad är x^2 + 4x + 39 = 19. Jag har
+            försökt använda pq-formeln men jag får inte...Jag har problem med denna frågan, vad är x^2 + 4x + 39 = 19. Jag har
+            försökt använda pq-formeln men jag får inte...Jag har problem med denna frågan, vad är x^2 + 4x + 39 = 19. Jag har
+            försökt använda pq-formeln men jag får inteJag har problem med denna frågan, vad är x^2 + 4x + 39 = 19. Jag har
+            försökt använda pq-formeln men jag får inte...Jag har problem med denna frågan, vad är x^2 + 4x + 39 = 19. Jag har
+            försökt använda pq-formeln men jag får inte...Jag har problem med denna frågan, vad är x^2 + 4x + 39 = 19. Jag har
+            försökt använda pq-formeln men jag får inte...Jag har problem med denna frågan, vad är x^2 + 4x + 39 = 19. Jag har
+            försökt använda pq-formeln men jag får inte.........`,
+      imgurl:
+        'https://www.incimages.com/uploaded_files/image/970x450/getty_470493341_20001333200092800_398689.jpg',
+    },
+    {
+      id: 3,
+      checkmark: true,
+      text: `Problem...`,
+      imgurl:
+        'https://cdn.mos.cms.futurecdn.net/xYiTisbsp2HZPVupAZoNYQ-650-80.jpg',
+    },
+  ];
+
+  const items = posts.map((post) => {
+    return (
+      <Item
+        key={post.id}
+        checkmark={post.checkmark}
+        text={post.text}
+        imgurl={post.imgurl}
+      />
+    );
+  });
+  return <div id="profile-wrapper">{items}</div>;
 }
