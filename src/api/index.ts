21c3778c6f6f467ea442ae6dc9f37eb092cf50dc
Måns Nilsson <mans.n@hotmail.com>
2020-05-06 21:36:05 +0200

    cookie array works

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,6 @@
 import io from 'socket.io-client';
 
-const socket = io('https://cd3ad028.ngrok.io');
+const socket = io('https://e27bccce.ngrok.io');
 
 export function getSubjects(cb) {
   socket.emit('get_channels', cb);
