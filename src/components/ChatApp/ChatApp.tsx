52e03eae1776ee0e9a050e2de57b99b5104150ef
Måns Nilsson <mans.n@hotmail.com>
2020-05-07 20:59:49 +0200

    checkmark works once

--- src/components/ChatApp/ChatApp.tsx
+++ src/components/ChatApp/ChatApp.tsx
@@ -6,7 +6,7 @@ import Chat from '../Chat';
 import LoadingIndicator from '../LoadingIndicator';
 import { useCookie } from '../../useCookie';
 
-export default function ChatApp() {
+export default function ChatApp(props) {
   const [name, setName] = useCookie('name');
   const [threadId, setThreadId] = useCookie('threadId');
   const [channelId, setChannelId] = useCookie('channelId');
@@ -52,7 +52,7 @@ export default function ChatApp() {
     let firstVisit = allChats === undefined;
     if (firstVisit) {
       setAllChats(
-        '{ "allThreadIds" : [], "allChannelIds" : [], "text" : [], "imageURL" : [], "checkbox" : []}'
+        '{ "allThreadIds" : [], "allChannelIds" : [], "text" : [], "imageURL" : [], "checkmark" : []}'
       );
     }
     setChannelId(subject.id);
@@ -82,11 +82,13 @@ export default function ChatApp() {
         allChatsObject.allChannelIds = allChatsObject.allChannelIds.concat(
           channelId
         );
-        allChatsObject.checkbox = allChatsObject.checkbox.concat(false);
+        allChatsObject.checkmark = allChatsObject.checkmark.concat(false);
         allChatsObject.text = allChatsObject.text.concat(text);
         allChatsObject.imageURL = allChatsObject.imageURL.concat(image);
         setAllChats(allChatsObject);
-        //console.log(allChats);
+        props.setIndex(allChatsObject.allThreadIds.length);
+        console.log(props.index);
+        console.log(allChats);
       }
     });
 
@@ -118,7 +120,12 @@ export default function ChatApp() {
     setMessages([]);
     api.cancelSession();
   }
-
+  function onCheckmark() {
+    let allChatsObject = allChats;
+    allChatsObject.checkmark[props.index] = true;
+    setAllChats(allChatsObject);
+    console.log(allChats);
+  }
   const subject = subjects.find((s) => s.id === channelId);
 
   const showPopup = !name && !loading;
@@ -139,6 +146,9 @@ export default function ChatApp() {
           messages={messages}
           onSendMessage={onSendMessage}
           onNewQuestionClick={onNewQuestion}
+          index={props.index}
+          allChats={allChats}
+          onCheckmarkClick={onCheckmark}
         />
       </div>
     </div>
