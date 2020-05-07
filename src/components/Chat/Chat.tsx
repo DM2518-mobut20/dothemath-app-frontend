58b7c7ea476f6a325933acd6f726f0ee40c5980c
Måns Nilsson <mans.n@hotmail.com>
2020-05-07 20:59:49 +0200

    checkmark works once

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -12,6 +12,9 @@ interface ChatProps {
   messages: OnMessageCallbackData[];
   onSendMessage(text: string, image?: File): any;
   onNewQuestionClick(): any;
+  index: number;
+  allChats: any;
+  onCheckmarkClick(): any;
 }
 
 export default function Chat(props: ChatProps) {
@@ -24,12 +27,19 @@ export default function Chat(props: ChatProps) {
           Ask New Question
         </button>
         <h2 id="subject-title">{props.subject?.name}</h2>
-        {true ? (
+        {props.allChats === undefined ? (
+          <button
+            id="chat-empty-checkmark"
+            className="profile-item-emptycheckmark"
+            onClick={props.onCheckmarkClick}
+          />
+        ) : props.allChats.checkmark[props.index] ? (
           <button id="chat-checkmark" className="profile-item-checkmark" />
         ) : (
           <button
             id="chat-empty-checkmark"
             className="profile-item-emptycheckmark"
+            onClick={props.onCheckmarkClick}
           />
         )}
       </div>
