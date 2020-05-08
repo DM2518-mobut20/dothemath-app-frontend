5f6b5504a9ddfd5bcfe34c028cf5d05786c1bcc6
Måns Nilsson <mans.n@hotmail.com>
2020-05-08 10:41:46 +0200

    added checkbox button
    checkmark works once
    
    
    Checkmark styling
    
    
    Ask New question -> new question
    
    
    commit innan radikalt test
    
    
    Checkbox works
    
    
    Inputbox dissapears when button is clicked
    
    
    removed commented out code
    
    
    Button is not clickable if no question was posted.
    
    
    fixed so that button is not clickable before question on first question

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -12,23 +12,58 @@ interface ChatProps {
   messages: OnMessageCallbackData[];
   onSendMessage(text: string, image?: File): any;
   onNewQuestionClick(): any;
+  index: number;
+  allChats: any;
+  onCheckmarkClick(): any;
 }
 
 export default function Chat(props: ChatProps) {
   const { messages } = props;
 
+  const emptyCheckmark = (
+    <button
+      id="chat-empty-checkmark"
+      className="profile-item-emptycheckmark"
+      onClick={props.onCheckmarkClick}
+    />
+  );
+  const checkedCheckmark = (
+    <button id="chat-checkmark" className="profile-item-checkmark" />
+  );
+  function displayCheckmark(bool) {
+    if (bool) {
+      return checkedCheckmark;
+    } else {
+      return emptyCheckmark;
+    }
+  }
+
+  function displayInputField(bool) {
+    if (bool) {
+      return <InputContainer onSend={props.onSendMessage} />;
+    }
+  }
   return (
     <div id="window-wrapper">
       <div id="title-container">
-        <h2 id="subject-title">{props.subject?.name}</h2>
         <button onClick={props.onNewQuestionClick} id="new-question-button">
-          Ask New Question
+          New Question
         </button>
+        <h2 id="subject-title">{props.subject?.name}</h2>
+        {props.allChats === undefined
+          ? displayCheckmark(false)
+          : props.allChats.checkmark[props.index]
+          ? displayCheckmark(true)
+          : displayCheckmark(false)}
       </div>
       <div id="content-wrapper">
         <div id="chat-wrapper">
           <ConversationContainer messages={messages} />
-          <InputContainer onSend={props.onSendMessage} />
+          {props.allChats === undefined
+            ? displayInputField(true)
+            : props.allChats.checkmark[props.index]
+            ? displayInputField(false)
+            : displayInputField(true)}
         </div>
       </div>
       <img
