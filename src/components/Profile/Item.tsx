5244001646671161168b2626e8b647ae61764252
Måns Nilsson <mans.n@hotmail.com>
2020-05-10 20:10:07 +0200

    added checkmark icon

--- src/components/Profile/Item.tsx
+++ src/components/Profile/Item.tsx
@@ -4,9 +4,31 @@ export default function Item(props) {
   return (
     <div className="profile-item" onClick={() => props.goToChat(props.index)}>
       {props.checkmark ? (
-        <div className="profile-item-checkmark" />
+        <div className="profile-item-checkmark">
+          <img
+            src="icons/check-mark-3-512.png"
+            alt="checkmark"
+            style={{
+              width: '30px',
+              display: 'table-cell',
+              margin: 'auto',
+              marginTop: '10px',
+            }}
+          />
+        </div>
       ) : (
-        <div className="profile-item-emptycheckmark" />
+        <div className="profile-item-emptycheckmark">
+          <img
+            src="icons/check-mark-3-512.png"
+            alt="checkmark"
+            style={{
+              width: '30px',
+              display: 'block',
+              margin: 'auto',
+              marginTop: '10px',
+            }}
+          />
+        </div>
       )}
       <div className="profile-item-text">
         <p>{props.text}</p>
