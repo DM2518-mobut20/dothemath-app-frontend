235f143aba30cb812e7336a1efef291b04355982
Måns Nilsson <mans.n@hotmail.com>
2020-05-10 20:10:07 +0200

    added checkmark icon

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -25,10 +25,22 @@ export default function Chat(props: ChatProps) {
       id="chat-empty-checkmark"
       className="profile-item-emptycheckmark"
       onClick={props.onCheckmarkClick}
-    />
+    >
+      <img
+        src="icons/check-mark-3-512.png"
+        style={{ width: '25px' }}
+        alt="checkmark-empty"
+      />
+    </button>
   );
   const checkedCheckmark = (
-    <button id="chat-checkmark" className="profile-item-checkmark" />
+    <button id="chat-checkmark" className="profile-item-checkmark">
+      <img
+        src="icons/check-mark-3-512.png"
+        alt="checkmark"
+        style={{ width: '25px' }}
+      />
+    </button>
   );
   function displayCheckmark(checked) {
     if (checked) {
