7c275f8911309433668c26ebb6b2c66940a5c208
Måns Nilsson <mans.n@hotmail.com>
2020-05-17 19:19:58 +0200

    removed default import

--- src/components/Tabbar/Tabbar.stories.tsx
+++ src/components/Tabbar/Tabbar.stories.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Tabbar from './Tabbar';
+import { Tabbar } from './Tabbar';
 
 export default {
   title: 'Tabbar/Tabbar',
--- src/components/Tabbar/Tabbar.tsx
+++ src/components/Tabbar/Tabbar.tsx
@@ -7,6 +7,7 @@ import ChatApp from '../ChatApp/ChatApp';
 import { useCookie } from '../../useCookie';
 import history from './history';
 import { ErrorBoundary } from '../ErrorBoundary';
+
 export function Tabbar() {
   const [index, setIndex] = useCookie('index');
   function setIndexOnNewMessage(indexNumber: number) {
